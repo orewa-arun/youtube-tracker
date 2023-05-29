409f4d1db05e1091f0280c66ed3f4887fb06790d
orewa-arun <arunworkacc0@gmail.com>
2023-05-29 12:37:07 +0530

    views from api

---
+++ fromapi.py
@@ -0,0 +1,42 @@
+import re
+from googleapiclient.discovery import build
+from dotenv import dotenv_values
+
+cloud_key = dotenv_values(".env")['GOOGLE_CLOUD_API_KEY']
+
+youtube = build('youtube', 'v3', developerKey=cloud_key)
+print(youtube)
+
+
+def get_video_views(video_id):
+    # Send a request to the API to retrieve video details
+    response = youtube.videos().list(
+        part='statistics',
+        id=video_id
+    ).execute()
+
+    # Extract the view count from the response
+    items = response.get('items', [])
+    if items:
+        view_count = items[0]['statistics']['viewCount']
+        return view_count
+
+
+def get_video_id(url):
+    # Regular expression pattern to match YouTube video ID
+    pattern = r'(?:v=|youtu\.be\/|v\/|\/v\/|youtu\.be\/|embed\/|watch\?v=|embed\/|watch\?feature=player_embedded&v=|watch\?v=)([\w-]+)'
+
+    # Find the video ID using the pattern
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    else:
+        print("Invalid YouTube URL.")
+
+
+# Replace with the actual YouTube video URL
+video_url = "https://www.youtube.com/watch?v=csM-ikF-fR4"
+video_id = get_video_id(video_url)
+views = get_video_views(video_id)
+if views:
+    print("Number of views:", views)
