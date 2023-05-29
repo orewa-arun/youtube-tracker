9b7bfcfbf4970bc0636b941b59012adc72b25633
orewa-arun <arunworkacc0@gmail.com>
2023-05-29 12:37:32 +0530

    views from scrapping

---
+++ scrapper.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import re as regex
+
+
+def scrape_view_count(video_url):
+    response = requests.get(video_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the script tag containing the JSON data
+        script_content = soup.find('body').find('script').string
+
+        match = regex.search(r'var\s+(\w+)\s+=\s+({.*?});', script_content)
+
+        if match:
+            # json_variable_name = match.group(1)
+            json_content = match.group(2)
+
+            # Parse the JSON content
+            data = json.loads(json_content)
+
+            # viewCount
+            view_count = data['videoDetails']['viewCount']
+
+            return view_count
+        else:
+            print("Failed to extract JSON data from script.")
+
+
+# Example usage
+# Replace VIDEO_ID with the actual video ID
+video_url = "https://www.youtube.com/watch?v=6JpJzygUrAw"
+views = scrape_view_count(video_url)
+if views:
+    print("Number of views:", views)
